af4f609555d5266edd2e06579bfc0e9033c69c16
Hakashi Katake <gammlast@gmail.com>
2024-12-01 00:46:00 +0530

    models and auths

--- DjangPractice/DjangPractice/settings.py
+++ DjangPractice/DjangPractice/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
