79cd5f720c3847e22735f26f8a798c8b7f15aa6f
Hakashi Katake <gammlast@gmail.com>
2024-12-01 17:59:00 +0530

    added models fetch to frontend

--- DjangPractice/practice/views.py
+++ DjangPractice/practice/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import ChaiVariety
 
 def all_chai(request):
-    return render(request, 'chai/all_chai.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'chai/all_chai.html', {'chais': chais})
 
 def order(request):
     return render(request, 'chai/order.html')
